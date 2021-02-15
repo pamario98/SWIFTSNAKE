2b8bed75022e085a0b92d2a478148b6062a18abe
Mario Eduardo Garcia Gonzalez <pamario98@hotmail.com>
2021-02-14 18:24:24 -0600

    initial commit

---
+++ snakebello/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  snakebello
+//
+//  Created by Mario Eduardo García González on 16/11/19.
+//  Copyright © 2019 Mario Eduardo García González. All rights reserved.
+//
+
+import Foundation
